aac1c618895f02e299914b42f2e8ddfcab5ad22a
Kanad Gharote <kanadgharote007@gmail.com>
2026-07-29 17:58:16 +0530

    Fix multi-primary cluster: separate data dirs and disable AOF to avoid file conflicts between Redis instances

--- plugin/trino-redis/src/test/java/io/trino/plugin/redis/util/RedisCluster.java
+++ plugin/trino-redis/src/test/java/io/trino/plugin/redis/util/RedisCluster.java
@@ -70,21 +70,24 @@ public class RedisCluster
         jedisSeedAddresses = new ArrayList<>(numPrimaries);
         seedAddresses = new ArrayList<>(numPrimaries);
 
-        // Build the command to start all Redis instances in a single container
+        // Build the command to start all Redis instances in a single container.
+        // Each instance gets its own data directory to avoid AOF/cluster-config file conflicts.
         StringBuilder command = new StringBuilder();
         List<Integer> ports = new ArrayList<>(numPrimaries);
         for (int i = 0; i < numPrimaries; i++) {
             int port = basePort + i;
             ports.add(port);
+            command.append("mkdir -p /data/").append(i).append(" && ");
             if (i > 0) {
                 command.append(" & ");
             }
             command.append("redis-server")
                     .append(" --port ").append(port)
                     .append(" --cluster-enabled yes")
-                    .append(" --cluster-config-file nodes").append(i).append(".conf")
+                    .append(" --cluster-config-file nodes.conf")
                     .append(" --cluster-node-timeout ").append(CLUSTER_TIMEOUT_MILLIS)
-                    .append(" --appendonly yes");
+                    .append(" --dir /data/").append(i)
+                    .append(" --appendonly no");
         }
         command.append(" & wait");
 

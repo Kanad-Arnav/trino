13190333fce47ee152a3446ad50374c16947b0f1
Kanad Gharote <kanadgharote007@gmail.com>
2026-07-30 18:22:35 +0530

    Fix TLS cluster: add --tls-cluster yes for encrypted cluster bus connections

--- plugin/trino-redis/src/test/java/io/trino/plugin/redis/util/RedisCluster.java
+++ plugin/trino-redis/src/test/java/io/trino/plugin/redis/util/RedisCluster.java
@@ -142,7 +142,10 @@ public class RedisCluster
             if (tls) {
                 command.append(" --tls-port ").append(port)
                         .append(" --port 0")
+                        .append(" --tls-cluster yes")
                         .append(" --cluster-port ").append(port + 10000)
+                        .append(" --cluster-announce-port ").append(port)
+                        .append(" --cluster-announce-bus-port ").append(port + 10000)
                         .append(" --tls-cert-file ").append(CONTAINER_CERTS_DIR).append("redis.crt")
                         .append(" --tls-key-file ").append(CONTAINER_CERTS_DIR).append("redis.key")
                         .append(" --tls-ca-cert-file ").append(CONTAINER_CERTS_DIR).append("ca.crt");
